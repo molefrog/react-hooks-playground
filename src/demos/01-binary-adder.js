c3cc6fc81a776fd275b4b94fbbb4c781c2a03da4
Alexey Taktarov <molefrog@gmail.com>
2021-11-16 07:11:05 -0800

    Binary Adder: bit flipper.

--- src/demos/01-binary-adder.js
+++ src/demos/01-binary-adder.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Label } from "../ui";
 
+const flip = (x) => (x ? 0b0 : 0b1);
+
 function BinaryAdder() {
-  const lhs = 0b1;
-  const rhs = 0b0;
+  const [lhs, setLhs] = useState(0b0);
+  const [rhs, setRhs] = useState(0b0);
 
   const sum = 0b0;
   const carry = 0b1;
 
   return (
     <>
-      <Button>{lhs}</Button>
+      <Button
+        switchedOn={lhs}
+        onClick={() => {
+          setLhs(flip(lhs));
+        }}
+      >
+        {lhs}
+      </Button>
       <Label>+</Label>
-      <Button>{rhs}</Button>
+      <Button switchedOn={rhs} onClick={() => setRhs(flip(rhs))}>
+        {rhs}
+      </Button>
       <Label>=</Label>
       <Label span={2}>{sum}</Label>
       <Label span={2}>{carry}</Label>

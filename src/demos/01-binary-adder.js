7a4c3e4a3144f2ee8ebd410405c234ba5548b223
Alexey Taktarov <molefrog@gmail.com>
2021-11-16 07:04:28 -0800

    Binary adder: basic static layout.

--- src/demos/01-binary-adder.js
+++ src/demos/01-binary-adder.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Button, Label } from "../ui";
 
 function BinaryAdder() {
-  return <></>;
+  const lhs = 0b1;
+  const rhs = 0b0;
+
+  const sum = 0b0;
+  const carry = 0b1;
+
+  return (
+    <>
+      <Button>{lhs}</Button>
+      <Label>+</Label>
+      <Button>{rhs}</Button>
+      <Label>=</Label>
+      <Label span={2}>{sum}</Label>
+      <Label span={2}>{carry}</Label>
+    </>
+  );
 }
 
 export default BinaryAdder;
